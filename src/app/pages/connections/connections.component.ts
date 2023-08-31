183dd3486c1bd17e080236033f3574399ecf6b57
Miguel Angel <ma.garcianoguera@hotmail.com>
2023-08-31 10:26:07 +0200

    A - Connections page

---
+++ src/app/pages/connections/connections.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-connections',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './connections.component.html',
+  styleUrls: ['./connections.component.css']
+})
+export class ConnectionsComponent {
+
+}
