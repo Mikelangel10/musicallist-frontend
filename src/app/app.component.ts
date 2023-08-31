d0df7a50fb9e6ed13a3e3a34abf86faf44c30446
Miguel Angel <ma.garcianoguera@hotmail.com>
2023-08-31 18:46:23 +0200

    U - Standalone Routes User-Profile, Favoritos, Conexiones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 import { FavoritesComponent } from './pages/favorites/favorites.component';
 import { ConnectionsComponent } from './pages/connections/connections.component';
 import { UserProfileComponent } from './pages/user-profile/user-profile.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [FavoritesComponent, ConnectionsComponent, UserProfileComponent],
+  imports: [
+    FavoritesComponent,
+    ConnectionsComponent,
+    UserProfileComponent,
+    RouterOutlet,
+  ],
 })
 export class AppComponent {
   title = 'Musicallist';
