bd37c18aedd11fb844ed4ba4bcf3c034ba607f50
Miguel Angel <ma.garcianoguera@hotmail.com>
2023-08-31 14:09:12 +0200

    U - Header & Footer Component Import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [],
+  imports: [FooterComponent, HeaderComponent],
 })
 export class AppComponent {
   title = 'Musicallist';
