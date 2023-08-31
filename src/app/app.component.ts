3b0687a84d6e1166462c0dcaa969f0e9e7343b3a
Miguel Angel <ma.garcianoguera@hotmail.com>
2023-08-31 13:51:01 +0200

    U - Import Favorites, Connections & UserProfile Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { FavoritesComponent } from './pages/favorites/favorites.component';
+import { ConnectionsComponent } from './pages/connections/connections.component';
+import { UserProfileComponent } from './pages/user-profile/user-profile.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [],
+  imports: [FavoritesComponent, ConnectionsComponent, UserProfileComponent],
 })
 export class AppComponent {
   title = 'Musicallist';
