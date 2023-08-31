6d2bc2529654ba16021ee735d665c7d5f457e00a
Miguel Angel <ma.garcianoguera@hotmail.com>
2023-08-31 19:00:54 +0200

    B - Bugs fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,17 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { FavoritesComponent } from './pages/favorites/favorites.component';
 import { ConnectionsComponent } from './pages/connections/connections.component';
 import { UserProfileComponent } from './pages/user-profile/user-profile.component';
 import { RouterOutlet } from '@angular/router';
-=======
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
->>>>>>> main
-=======
-import { FooterComponent } from './shared/footer/footer.component';
-import { HeaderComponent } from './shared/header/header.component';
->>>>>>> 13c68b2 (U - Standalone Routes User-Profile, Favoritos, Conexiones)
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-<<<<<<< HEAD
-<<<<<<< HEAD
+
   imports: [
     FavoritesComponent,
     ConnectionsComponent,
@@ -29,11 +20,10 @@ import { HeaderComponent } from './shared/header/header.component';
   ],
 =======
   imports: [FooterComponent, HeaderComponent],
->>>>>>> main
-=======
+
   imports: [FooterComponent, HeaderComponent],
->>>>>>> 13c68b2 (U - Standalone Routes User-Profile, Favoritos, Conexiones)
-})
+
+
 export class AppComponent {
   title = 'Musicallist';
   name = 'Miguel';
