7ef54173c4773ef4043ad04822d80e3209d964de
Miguel Angel <ma.garcianoguera@hotmail.com>
2023-08-31 18:46:23 +0200

    U - Standalone Routes User-Profile, Favoritos, Conexiones

--- src/app/app.component.routing.ts
+++ src/app/app.component.routing.ts
@@ -1,10 +1,34 @@
 import { Routes } from '@angular/router';
+import { UserProfileComponent } from './pages/user-profile/user-profile.component';
+import { FavoritesComponent } from './pages/favorites/favorites.component';
+import { ConnectionsComponent } from './pages/connections/connections.component';
 
 export const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
     loadComponent: () => import('./app.component').then((m) => m.AppComponent),
+  },
+  {
+    path: 'usuario',
+    loadComponent: () =>
+      import('../app/pages/user-profile/user-profile.component').then(
+        (c) => c.UserProfileComponent
+      ),
+  },
+  {
+    path: 'favoritos',
+    loadComponent: () =>
+      import('../app/pages/favorites/favorites.component').then(
+        (c) => c.FavoritesComponent
+      ),
+  },
+  {
+    path: 'conexiones',
+    loadComponent: () =>
+      import('../app/pages/connections/connections.component').then(
+        (c) => c.ConnectionsComponent
+      ),
   },
   {
     path: '**',
