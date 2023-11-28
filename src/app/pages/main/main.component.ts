7b8383d5a37dfde8cc3eead1d45ee0a2cb6dab2a
Miguel Angel <ma.garcianoguera@hotmail.com>
2023-11-28 21:05:00 +0100

    U  - Import a new component for main.component.ts

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import { AppInfoLinksAndImagesComponent } from 'src/app/components/app-info-links-and-images/app-info-links-and-images.component';
+import { InfoLinksAndImagesComponent } from 'src/app/components/info-links-and-images/info-links-and-images.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule, RouterLink, AppInfoLinksAndImagesComponent],
+  imports: [CommonModule, RouterLink, InfoLinksAndImagesComponent],
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css'],
 })
